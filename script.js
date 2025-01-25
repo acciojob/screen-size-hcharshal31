ec2f2e880c0ad0ce185507c98fe786984f2d8d2d
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-26 01:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function updateHeight(){
+	let width = window.innerWidth;
+	let height= window.innerHeight;
+	
+	document.getElementById("width").textContent = width;
+	document.getElementById("height").textContent = height;
+}
+updateHeight();
+window.addEventListner("resize", updateHeight);
+
